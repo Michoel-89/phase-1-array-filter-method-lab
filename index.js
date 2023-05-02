26e446316b75af05a9d9ae20c794ed0a301954b5
michoel <michoelr111@gmail.com>
2023-05-02 17:00:26 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(array, str) {
+    let nArr = [];
+    array.filter(function(i) {
+        if (i.toLowerCase() === str.toLowerCase()) {
+            nArr.push(i);
+        }
+    })
+    return nArr;
+}
+function fuzzyMatch(array, str) {
+    let nArr = [];
+    array.filter(function(i) {
+        if (i[0].toLowerCase() === str[0].toLowerCase()) {
+            nArr.push(i);
+        }
+    })
+    return nArr;
+}
+function matchName(array, str) {
+    let nArr = [];
+    array.filter(function(i) {
+        if(i.name === str) {
+          nArr.push(i)
+        }
+    })
+    return nArr;
+}
